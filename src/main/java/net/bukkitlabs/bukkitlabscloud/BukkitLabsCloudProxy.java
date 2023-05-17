dabf34396b04e6ff5330b441bfbdf4cd8e629c66
Asedem <asedem.games@gmail.com>
2023-05-17 21:57:19 +0200

    Initial commit

---
+++ src/main/java/net/bukkitlabs/bukkitlabscloud/BukkitLabsCloudProxy.java
@@ -0,0 +1,16 @@
+package net.bukkitlabs.bukkitlabscloud;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class BukkitLabsCloudProxy extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
